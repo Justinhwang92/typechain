d806a56f631f5a6ba0d9ac8565960fe4d878e5ac
Justin <justinhwang92@gmail.com>
2021-08-20 17:02:40 -0400

    Interface on TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender?): string => {
-  return `hello ${name}, you are ${age}, you are ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Justin",
+  age: 29,
+  gender: "male",
 };
 
-console.log(sayHi("Justin", 29, "male"));
+const sayHi = (person: Human): string => {
+  return `hello ${person.name}, you are ${person.age}, you are ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {};
