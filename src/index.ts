0de5357b417cccb495919ee81bb4dbd57729f5bf
Justin <justinhwang92@gmail.com>
2021-08-21 16:23:57 -0400

    Add createNewBlock function

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,29 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+
+  const newHash: string = Block.calculateHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+
 export {};
